f2b746f3834f6d45a8794b7e42ad663cad2a3909
garg81 <grohit8127@gmail.com>
2025-09-03 12:52:08 +0530

    proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,9 +3,11 @@ export default async function handler(req, res) {
         return res.status(405).json({ message: 'Only POST requests are allowed' });
     }
 
+    // ▼▼▼ YEH TEEN LINES SABSE ZAROORI HAIN ▼▼▼
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // ▲▲▲ YEH TEEN LINES SABSE ZAROORI HAIN ▲▲▲
 
     try {
         const { keyIndex, prompt } = req.body;
